ca357f88e8778c0537aed9078399a900492daad9
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-11-28 18:51:31 +0600

    latest news section added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,7 @@ import Banner from "@/components/banner/Banner";
 import Testimonial from "./testimonials/Testimonials";
 import Footer from "./Footer/footer";
 import InSchoolLan from "./InSchoolNum/InSchoolLan";
+import LatestNews from "./LatestNews/LatestNews";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Navbar />
             <Banner />
             <InSchoolLan />
+            <LatestNews />
             <Testimonial></Testimonial>
             <Footer></Footer>
         </div>
